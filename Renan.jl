1ff00dbda12918e7b768ba0a55d7119deead75b6
Debashmita Poddar <32616508+debashmitap@users.noreply.github.com>
2021-03-18 12:08:02 +0100

    Add files via upload

--- Renan.jl
+++ Renan.jl
@@ -1,12 +1,17 @@
 using Luxor
 
 r = Turtle(0.0, 0.0, true, 0.0, (0.20, 1.0, 0.50))
-Drawing(1000, 1000, "Renan.png")
+fontsize(500)
 
 @svg begin
-    Reposition(r, -50, -100)
-    Message(r,"Happy Birthday")
-    Reposition(r, -230, 100)
+    Reposition(r, -250, -100)
+    fontsize(55)
+    Message(r,"Happy 27th Birthday,")
+    Penwidth(r, 4)
+    Pencolor(r, "red")
+    Reposition(r, -240, 100)
+
+    #The R
     Turn(r, -90)
     Forward(r, 100)
     Turn(r, 180)
@@ -24,11 +29,15 @@ Drawing(1000, 1000, "Renan.png")
     Forward(r, 50)
     Turn(r, 90)
     Forward(r, 50)
+
+    #The first space
     Penup(r)
     Turn(r, -180)
     Forward(r, 100)
     Turn(r, 90)
     Forward(r, 50)
+
+    #The E
     Pendown(r)
     Turn(r, 180)
     Forward(r, 100)
@@ -46,8 +55,12 @@ Drawing(1000, 1000, "Renan.png")
     Forward(r, 50)
     Turn(r, -90)
     Forward(r, 50)
+
+    #The second space
     Penup(r)
     Forward(r, 50)
+
+    #The first N
     Pendown(r)
     Turn(r, -90)
     Forward(r, 100)
@@ -55,9 +68,13 @@ Drawing(1000, 1000, "Renan.png")
     Forward(r, 110)
     Turn(r, -155)
     Forward(r, 100)
+
+    #The third space
     Penup(r)
     Turn(r, 90)
     Forward(r, 90)
+
+    #The A
     Pendown(r)
     Turn(r, 70)
     Forward(r, 110)
@@ -69,11 +86,15 @@ Drawing(1000, 1000, "Renan.png")
     Forward(r, 55)
     Turn(r, 70)
     Forward(r, 39)
+
+    #4th space
     Penup(r)
     Turn(r, 90)
     Forward(r, 52)
     Turn(r, -90)
     Forward(r, 70)
+
+    #The second N
     Pendown(r)
     Turn(r, -90)
     Forward(r, 100)
